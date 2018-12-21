906179eae8be0ffd0a7a226723a44a28f6a2740b
MacroLin <1074863419@qq.com>
2018-12-21 23:43:15 +0800

    make build again

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const Koa = require('koa')
+const path = require('path')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
+const koaStatic = require('koa-static')
 const routers = require('./routers')
 
 const app = new Koa()
 const router = new Router()
 
+app.use(koaStatic(path.join(__dirname,'public','build')))
 app.use(bodyParser())
 app.use(routers.routes(),routers.allowedMethods())
 
